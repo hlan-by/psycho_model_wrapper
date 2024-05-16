13144fece70705e6566737dd9ef5572bb5f971cf
ahladkevich <MyWF@21t>
2024-05-16 12:50:32 +0300

    ExpansionUrgeBasic fix

--- src/main/java/urges/ExpansionUrgeBasic.java
+++ src/main/java/urges/ExpansionUrgeBasic.java
@@ -1,4 +1,14 @@
 package urges;
 
 public class ExpansionUrgeBasic implements Urge {
+    private double impact;
+
+    public ExpansionUrgeBasic(double impact) {
+        this.impact = impact;
+    }
+
+    @Override
+    public double getImpact() {
+        return impact;
+    }
 }
